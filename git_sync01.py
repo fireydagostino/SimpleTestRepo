010e25e3b2a911378b1312457b2b1e310b6f9589
Anthony D <anthony.dagostino@bell.ca>
2018-02-09 14:41:46 -0500

    Added additional syntax to the git_sync01.py file

--- git_sync01.py
+++ git_sync01.py
@@ -1,4 +1,13 @@
 import subprocess
 
+'''
+    Note: The bash script being run is dependent on the remote
+    repository being set as the upstream for ease when using
+    the git rev-parse command.
+    
+    If it is not the case, perform an initial set with the
+    following command: git remote add upstream
+'''
+
 cmd = "./sync_prog.sh"
 sync_check = subprocess.check_call(["bash", "-c", cmd])
