0dd14f1275bf7f588908542335f4aada1711180c
Anthony D <anthony.dagostino@bell.ca>
2018-02-09 15:30:38 -0500

    Added additional comments into git_sync01.py
    Comments primarily pertain to an evolutionary todo list.

--- git_sync01.py
+++ git_sync01.py
@@ -1,13 +1,24 @@
+"""
+    This python code is used to run the bash script sync_prog.sh
+    which performs a sync check with the designated upstream
+    remote branch.
+"""
+
 import subprocess
 
 '''
     Note: The bash script being run is dependent on the remote
     repository being set as the upstream for ease when using
     the git rev-parse command.
-    
-    If it is not the case, perform an initial set with the
-    following command: git remote add upstream
+    If this is not the case, perform an initial set with the
+    following command: git branch --set-upstream-to=origin/<branch> master
 '''
 
 cmd = "./sync_prog.sh"
 sync_check = subprocess.check_call(["bash", "-c", cmd])
+
+'''
+    Note: add additional code that enables the sync check to be run as a
+    method, where the input is the remote url to sync up with. Therefore,
+    this will be able to auto-sync with any desired remote repository.
+'''
