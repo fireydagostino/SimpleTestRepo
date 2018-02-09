09fe9f3adfb58d04c5515c3018af81d64e372f3e
Anthony D <anthony.dagostino@bell.ca>
2018-02-09 13:40:08 -0500

    Configured echo.sh to test for repository matches
    Updated the echo.sh file to test for local and remote
    repository matches.
    
    This was done within the bash script and run through
    the python script bash01.py.
    The primary command used was git rev-parse @ for local
    and git rev-parse @{u} for the remote.

--- echo.sh
+++ echo.sh
@@ -24,3 +24,24 @@ git status
 git checkout master
 git branch -d experimental
 git branch
+
+git remote update
+
+git rev-parse @ > hash_list.txt
+git rev-parse @{u} >> hash_list.txt
+cat hash_list.txt
+rm hash_list.txt
+
+local_repo=$(git rev-parse @)
+remote_repo="$(git rev-parse @{u})"
+
+if [[ "$local_repo" == "$remote_repo" ]]; then
+    echo "The two repositories are matched"
+else
+    echo "The local and remote repositories are not synced up."
+    sync=true
+fi
+
+if [ "$sync" = true ]; then
+    echo "Preparing to sync up repositories..."
+fi
