3aab04a6a512f5c97b40776c5f40b68311f5eaf0
Anthony D <anthony.dagostino@bell.ca>
2018-02-12 11:13:14 -0500

    Created git_syncv1.0.py
    This is a cleaned up version of git_sync01.py
    Included is a better name variable which includes
    the version number for future updates to the scripts.

---
+++ git_sync_v1.0.py
@@ -0,0 +1,21 @@
+"""
+    Python script designed to sync up the current repository
+    with the selected remote repository (in this case the
+    sigma project by Neo23x0).
+    Sigma URL: https://github.com/Neo23x0/sigma.git
+"""
+
+import subprocess
+
+
+'''
+    Python method to run a bash script within python.
+    In this case the bash script to run is called
+    sync_prog.sh
+'''
+def run_sync():
+    subprocess.check_call(["bash", "-c", "./sync_prog.sh"])
+
+
+if __name__ == '__main__':
+    run_sync()
